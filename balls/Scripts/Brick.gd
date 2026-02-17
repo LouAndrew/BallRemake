532995748a76cec13eab81c4283af7269691ef32
LouAndrew <louisandrew324@gmail.com>
2026-02-17 12:12:20 +0300

    smoothing up

--- balls/Scripts/Brick.gd
+++ balls/Scripts/Brick.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var fall:bool=false
 var gravity:int = 1800
 var velocity:Vector2 = Vector2.ZERO
-onready var timer = $Timer
+
 func _physics_process(delta: float) -> void:
 	detectCircle()
 	floorDetect()
@@ -12,16 +12,14 @@ func _physics_process(delta: float) -> void:
 	velocity = move_and_slide(velocity)
 	velocity = move_and_slide(velocity,Vector2.UP)
 func _ready() -> void:
-	timer.connect("timeout",self,"_on_timer_out")
-func _on_timer_out():
-	SelfDelete()
+	pass
 	
 func floorDetect():
 	if is_on_floor():
-		timer.wait_time = 2.0
-		timer.start()
+		SelfDelete()
 func detectCircle():
 	if $RayCast2D.is_colliding():
 		fall = true
 func SelfDelete():
+	yield(get_tree().create_timer(1.0),"timeout")
 	queue_free()
--- balls/Scripts/levelTutorialCode.gd
+++ balls/Scripts/levelTutorialCode.gd
@@ -1,42 +1,51 @@
 extends Node
 
-onready var action1 = $inGameAction
-onready var action2 = $inGameAction2
-onready var textbg = $textBg
-
 onready var player = get_tree().get_nodes_in_group("player")[0]
-
 
 var triggerPos:Array = [-1000,-289]
 var triggNum: int = 0
 var desiredInput = Input.is_action_just_pressed("ui_right")
 var pressed:bool = false
 var displayedGuids:Array = [false,false]
-func _ready() -> void:
-	pass
-func _process(delta: float) -> void:
-	introduceMechanic(delta,action1,textbg,Vector2(530,504),triggerPos[0],Input.is_action_just_pressed("ui_right"),displayedGuids[0])
-	#introduceMechanic(delta,action2,textbg,Vector2(598,390),triggerPos[1],Input.is_action_just_pressed("ui_up"),displayedGuids[1])
+
+var basicHelp:Array = [
+	"Press Q to go left",
+	"Press E to go right",
+	"Press W to Jump",
+	"Press S to Duck"
+	]
+var advancedHelp:Array = [
+	"To jump over obstacles, jump and hit the left or right button",
+	"To dodge low obstacles press S to duck, to fit through",
+	"To pass large gaps use ramps to gain speed to cover them",
+	"Use the color guides to plan your next move",
+	"To change direction mid air release button and press the opposite button",
+	"avoid falling bricks and spikes"
+	]
 	
-func introduceMechanic(delta,action:Sprite,text:Sprite,actionTargetPos:Vector2,triggerDist:float,input,displayed):
-	if LevelMonitor.currentLevel == 0 and player.global_position.x >= triggerDist and ! displayed:
-		var showGuides:bool  = false
-		if showGuides:
-			Engine.time_scale = 0.1
-			action.visible = true
-			text.visible = true
-			interpolateEl(delta,actionTargetPos,action)
-		if input:
-			Engine.time_scale = 1
-			pressed = true
-			showGuides = false
-			action.visible = false
-			text.visible = false
-			displayed = true
-		if player.global_position.x >= triggerDist and ! pressed:
-			showGuides = true
-			
+onready var helpButton:Button = $helpButton
+
+func _ready() -> void:
+	helpButton.connect("pressed",self,'onHelpButtonPressed')
+	
+func _process(delta: float) -> void:
+	detectMouseDist(delta)
+	
+func detectMouseDist(delta):
+	if $helpButton/Sprite.global_position.distance_to($helpButton/Sprite.get_global_mouse_position()) <= 50:
+		interpolateElScale(delta,Vector2(0.98,0.88),$helpButton/Sprite)
+	else:
+		interpolateElScale(delta,Vector2(0.844,0.757),$helpButton/Sprite)
+		
+func onHelpButtonPressed():
+	$text.visible = true
+	
 func interpolateEl(delta,targetPos:Vector2,element):
 	var t = 0.07
 	t += delta * 0.4
 	element.global_position = element.global_position.linear_interpolate(targetPos,t)
+	
+func interpolateElScale(delta,targetPos:Vector2,element):
+	var t = 0.2
+	t += delta * 0.4
+	element.scale = element.scale.linear_interpolate(targetPos,t)
